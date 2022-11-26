78b5aa28124c0efc8bb5623183fc493819cb6b89
AlexanderSub <subbotin_aleks@mail.ru>
2022-11-26 15:00:26 +0300

    test library version

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Button} from './button'
 import renderer from 'react-test-renderer'
 
-
 describe('Button', () => {
   it('matches snapshot', () => {
     const tree = renderer.create(
