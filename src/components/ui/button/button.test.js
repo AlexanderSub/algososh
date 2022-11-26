ab9c0ce62aa5ac3d52673bfed116c1615d65ad7d
AlexanderSub <subbotin_aleks@mail.ru>
2022-11-26 11:59:26 +0300

    test structure

---
+++ src/components/ui/button/button.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from './button'
+import renderer from 'react-test-renderer'
+
+
+describe('Button', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(
+      <Button/>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
